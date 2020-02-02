132e9d6b2abeabb715a6f45c5dda3eb093fde931
anthonyzero <736252868@qq.com>
2020-02-02 16:15:37 +0800

    翻转二叉树

---
+++ Recursion/Invert-Binary-Tree/src/Solution.java
@@ -0,0 +1,26 @@
+/// 226. Invert Binary Tree
+/// https://leetcode.com/problems/invert-binary-tree/description/
+/// 时间复杂度: O(n), n为树中节点个数
+/// 空间复杂度: O(h), h为树的高度
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        //交换
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
